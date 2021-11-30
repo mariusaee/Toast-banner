028aa3aed6cfe6cab22ac4d80799a0dfe0b3b9d0
mariusaee <mariusae@me.com>
2021-11-30 14:42:09 +0300

    Initial Commit

---
+++ Toast banner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Toast banner
+//
+//  Created by Marius Malyshev on 30.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Toast banner/Toast_bannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toast_bannerApp.swift
+//  Toast banner
+//
+//  Created by Marius Malyshev on 30.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Toast_bannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
