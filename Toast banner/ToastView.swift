f72c45eedf973ff6e5b3d63f0b99cf1e01e2f0fc
mariusaee <mariusae@me.com>
2021-12-01 16:58:23 +0300

    -

--- Toast banner/ToastView.swift
+++ Toast banner/ToastView.swift
@@ -8,38 +8,48 @@
 import SwiftUI
 
 struct ToastView: View {
+    @State private var isShowing = false
+    
     var body: some View {
         ZStack {
             Text("Hello, World!")
-            
+                .toast(isShowing: $isShowing)
+                .onTapGesture {
+                    isShowing.toggle()
+                }
         }
     }
 }
 
 struct ToastModifier: ViewModifier {
+    @Binding var isShowing: Bool
+    let duration: TimeInterval
+    
     func body(content: Content) -> some View {
         ZStack {
             content
-            VStack {
-                Spacer()
-                HStack {
-                    Image(systemName: "heart.fill")
-                    Text("Tim Cook liked your post")
+            if isShowing {
+                VStack {
                     Spacer()
+                    HStack {
+                        Image(systemName: "heart.fill")
+                        Text("Tim Cook liked your post")
+                        Spacer()
+                    }
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(50)
+                    .shadow(radius: 5)
                 }
                 .padding()
-                .background(Color.white)
-                .cornerRadius(50)
-                .shadow(radius: 5)
             }
-            .padding()
         }
     }
 }
 
 extension View {
     func toast(isShowing: Binding<Bool>, duration: TimeInterval = 3) -> some View {
-        modifier(ToastModifier())
+        modifier(ToastModifier(isShowing: isShowing, duration: duration))
     }
 }
 
