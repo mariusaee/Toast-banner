550893c728800c37238e26cb38d9453ad4545d7f
mariusaee <mariusae@me.com>
2021-12-01 16:26:32 +0300

    Toast created

---
+++ Toast banner/ToastView.swift
@@ -0,0 +1,39 @@
+//
+//  ToastView.swift
+//  Toast banner
+//
+//  Created by Marius Malyshev on 01.12.2021.
+//
+
+import SwiftUI
+
+struct ToastView: View {
+    var body: some View {
+        ZStack {
+            Text("Hello, World!")
+            VStack {
+                Spacer()
+                HStack {
+                    Image(systemName: "heart.fill")
+                    Text("Tim Cook liked your post")
+                    Spacer()
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(50)
+                .shadow(radius: 5)
+            }
+            .padding()
+        }
+    }
+}
+
+struct ToastView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ToastView()
+            ToastView()
+                .previewDevice("iPhone SE (1st generation)")
+        }
+    }
+}
