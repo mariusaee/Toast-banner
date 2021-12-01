d170a41e8006a6f5b9120a2ae42598b5bad4681d
mariusaee <mariusae@me.com>
2021-12-01 15:43:25 +0300

    Toast by video

--- Toast banner/ContentView.swift
+++ Toast banner/ContentView.swift
@@ -7,10 +7,56 @@
 
 import SwiftUI
 
+struct ToastModifier: ViewModifier {
+    @Binding var isShowing: Bool
+    let duration: TimeInterval
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+            if isShowing {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Image(systemName: "heart.fill")
+                        Text("Post saved successfully")
+                        Spacer()
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(5)
+                    .shadow(radius: 5)
+                }
+                .padding()
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+                        withAnimation {
+                            isShowing = false
+                        }
+                    }
+                }
+            }
+           
+        }
+    }
+}
+
+extension View {
+    func toast(isShowing: Binding<Bool>, duration: TimeInterval = 3) -> some View {
+        modifier(ToastModifier(isShowing: isShowing, duration: duration))
+    }
+}
+
 struct ContentView: View {
+    @State private var isShowingToast = false
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .onTapGesture {
+                isShowingToast.toggle()
+            }
+            .toast(isShowing: $isShowingToast)
     }
 }
 
